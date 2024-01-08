10ea40e3759af428786112db8d60e85b845a0f7a
ARAYNAUD <adrien.raynaud@ynov.com>
2024-01-08 18:25:27 +0100

    a

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
 const port = 3030;
+
+// Configuration de la connexion à la base de données
+const db = mysql.createConnection({
+  host: 'mariadb',
+  user: 'adri',
+  password: 'adri',
+  database: 'Back'
+});
+
+// Connexion à la base de données
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+  } else {
+    console.log('Connecté à la base de données');
+  }
+});
 
 // Route principale
 app.get('/', (req, res) => {
