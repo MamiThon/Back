3f300765f7274f64a5682fc5fde9f19e40e5e283
Maxime Chevalier <90835260+MaxChevalier@users.noreply.github.com>
2024-01-16 12:33:31 +0100

    🔒️ bypass CORS

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const port = 3030;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// bypass CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 // Utilisez uniquement Sequelize pour la gestion de la base de données
 sequelize.sync({ force: false })
   .then(() => {
